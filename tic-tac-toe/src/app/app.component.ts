977c86c45fca36beb932eb2fd2158c7e41d2f904
Valentin Patrona <valytruewarlock@gmail.com>
2023-03-28 23:16:40 +0300

    some styling

--- tic-tac-toe/src/app/app.component.ts
+++ tic-tac-toe/src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tic-tac-toe';
+
+  grid:any=[
+    [null,null,null],
+    [null,null,null],
+    [null,null,null]
+  ]
+
+
 }
